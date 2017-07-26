e39365696b4d2d0d2f0ecf6b9c90591bff7c40df
jerry.deng <jerry.deng@greaconsulting.com>
2017-07-26 18:03:48 +0800

    单元测试

---
+++ src/test/java/test/jerry/sph/controller/ActionControllerTest.java
@@ -0,0 +1,43 @@
+package test.jerry.sph.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import jerry.sph.controller.ActionController;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class ActionControllerTest {
+	private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new ActionController()).build();
+    }
+    
+    @Test
+    public void home() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+
+    @Test
+    public void user() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/jerry").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello,jerry")));
+    }
+
+}
